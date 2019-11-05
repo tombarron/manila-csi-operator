38e65c42dd9c3d4f949db39f1ae7cca81b84d089
Mike Fedosin <mfedosin@redhat.com>
2019-11-05 16:58:35 +0100

    Add manilacsi API to the operator

---
+++ pkg/apis/manilacsi/v1alpha1/manila_types.go
@@ -0,0 +1,51 @@
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// ManilaSpec defines the desired state of Manila
+// +k8s:openapi-gen=true
+type ManilaSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+}
+
+// ManilaStatus defines the observed state of Manila
+// +k8s:openapi-gen=true
+type ManilaStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// Manila is the Schema for the manilas API
+// +k8s:openapi-gen=true
+// +kubebuilder:subresource:status
+// +kubebuilder:resource:path=manilas,scope=Namespaced
+type Manila struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   ManilaSpec   `json:"spec,omitempty"`
+	Status ManilaStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// ManilaList contains a list of Manila
+type ManilaList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Manila `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Manila{}, &ManilaList{})
+}
